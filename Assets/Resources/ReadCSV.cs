61fcb5d2b2d7a378cb08cdf98350736a4830e049
JakobHS <sharpejh00@gmail.com>
2025-02-20 05:14:30 +0100

    bs
    first tests of readng exel

--- Assets/Resources/ReadCSV.cs
+++ Assets/Resources/ReadCSV.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using TMPro;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ReadCSV : MonoBehaviour
 {
+
+    public TextAsset TextAssetData;
+    public TMP_InputField TextInput;
+    private string Text;
+
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI DoB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,19 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Text = TextInput.text;
+    }
+
+    public void Search()
+    {
+        string[] data = TextAssetData.text.Split(new string[] { ";", "\n" }, System.StringSplitOptions.None);
+        for (int i = 0; i<data.Length; i++)
+        {
+            if (Text == data[i])
+            {
+                Name.text = data[i + 1];
+                DoB.text = data[i + 2];
+            }
+        }
     }
 }
