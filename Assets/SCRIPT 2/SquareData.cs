96f519ef6ad2b6fcca9297e63a98b524ea4e9afa
Galionare <galion1325@gmail.com>
2025-03-18 16:40:56 +0000

    DOne

--- Assets/SCRIPT 2/SquareData.cs
+++ Assets/SCRIPT 2/SquareData.cs
@@ -22,16 +22,16 @@ public class SquareData : MonoBehaviour
     }
     private void DisplayTileData(PropertyData data)
     {
-        Name.text = $"Name: {data.NameProperty}";
+        Name.text = $"{data.NameProperty}";
         if (data.CanBeBought)
         {
-            Price.text = $"Price: £{data.Price}";
+            Price.text = $"£{data.Price}";
             Price.gameObject.SetActive(true);
             Action.gameObject.SetActive(false);
         }
         else
         {
-            Action.text = $"Action: {data.Action}";
+            Action.text = $"{data.Action}";
             Price.gameObject.SetActive(false);
             Action.gameObject.SetActive(true);
         }
