11ddfdef45fdbfd891dc336ff360feda621b0144
Noahhx <87083494+Noahhx@users.noreply.github.com>
2025-03-19 13:40:44 +0000

    ALL DICE SHIT IS DONE
    3 Dice Scripts + Added Dice Gameobject fully fleshed out ready to let emil use it in the character shi

---
+++ Assets/DiceAssets/DiceRolling.cs
@@ -0,0 +1,49 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using System.Linq;
+
+public class DiceRolling : MonoBehaviour
+{ 
+    public Vector3 direction = new Vector3(1, 1, 0); //throws upwards and to the side
+    private Rigidbody rb;
+    public int diceNumber;
+    private SphereCollider[] colliders;
+    void Awake()
+    {
+        diceNumber = 0;
+        transform.rotation = Random.rotation; //starts the throw in a random direction effectively, could probably randmoise the vectors but this is cleaner
+        //transform.position = direction * Time.deltaTime; (was always throwing from world origin? probably timedelta starting at 0?)
+        rb.AddForce(direction * 999, ForceMode.Impulse); //the throw itself, adding velocity effectively using direction * force 
+        
+    }
+    void Update()
+    {
+        if (rb.linearVelocity == Vector3.zero)
+        {
+            float maxY = colliders.Max(c => c.transform.position.y);
+           switch (maxY) {
+                case 1:
+                     diceNumber = 1;
+                     break;
+                case 2:
+                     diceNumber = 2;
+                     break;
+                case 3:
+                     diceNumber = 3;
+                     break;
+                case 4:
+                     diceNumber = 4;
+                     break;
+                case 5:
+                     diceNumber = 5;
+                     break;
+                case 6:
+                     diceNumber = 6;
+                     break;
+              }
+        }
+    }
+    
+    //Some Method That Returns the top facing number, be it using raycast or having a collision on the opposite side labelled with its number
+    //only AFTER velocity is 0 and NOT when velocity is 0 in the starting frame, sleep for 1ms is a conditional fix that will serve fine
+}
