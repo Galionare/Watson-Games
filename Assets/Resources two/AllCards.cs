b1f739e17712dce134b235406ddbd21faee2d4d6
JakobHS <sharpejh00@gmail.com>
2025-04-03 14:06:26 +0200

    View All Cards complete

--- Assets/Resources two/AllCards.cs
+++ Assets/Resources two/AllCards.cs
@@ -19,11 +19,13 @@ public class AllCards : MonoBehaviour
     public void ViewCards()
     {
         propertyData = CSVLoader.LoadPropertyData();
+        int Counter1 = 0;
+        int Counter2 = 0;
+        int Counter3 = 0;
 
         for (int i = 1; i <= 40; i++)
         {
             int Position = i;
- 
             if (propertyData.TryGetValue(Position, out PropertyData data))
             {
                 
@@ -32,20 +34,42 @@ public class AllCards : MonoBehaviour
                     GameObject Card1 = Instantiate(PropCard, Canvas.transform) as GameObject;
                     Card1.GetComponent<StreetCard>().CreateCard(Position);
 
+
+                    if (Counter1 <= 7)
+                    {
+                        RectTransform rectTransform = Card1.GetComponent<RectTransform>();
+                        rectTransform.anchoredPosition = new Vector2(-600, 320-(Counter1*100));
+                    }
+
+                    else if(Counter1 <= 13)
+                    {
+                        RectTransform rectTransform = Card1.GetComponent<RectTransform>();
+                        rectTransform.anchoredPosition = new Vector2(-300, 320 - ((Counter1-8) * 100));
+                    }
+                    else
+                    {
+                        RectTransform rectTransform = Card1.GetComponent<RectTransform>();
+                        rectTransform.anchoredPosition = new Vector2(0, 320 - ((Counter1 - 14) * 100));
+                    }
+                    Counter1++;
                 }
 
                 if (data.CanBeBought && (data.Group.Contains("Station")))
                 {
                     GameObject Card2 = Instantiate(StatCard, Canvas.transform) as GameObject;
                     Card2.GetComponent<StationCard>().CreateCard(Position);
-
+                    RectTransform rectTransform = Card2.GetComponent<RectTransform>();
+                    rectTransform.anchoredPosition = new Vector2(350, 320 - (Counter2 * 200));
+                    Counter2++;
                 }
 
                 if (data.CanBeBought && (data.Group.Contains("Utilities")))
                 {
                     GameObject Card3 = Instantiate(UtilCard, Canvas.transform) as GameObject;
                     Card3.GetComponent<UtilityCard>().CreateCard(Position);
-
+                    RectTransform rectTransform = Card3.GetComponent<RectTransform>();
+                    rectTransform.anchoredPosition = new Vector2(700, 320 - (Counter3 * 200));
+                    Counter3++;
                 }
             }
         }
