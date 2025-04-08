169357ae3fd91e57c93731c2c60b9cd39f364bba
Noahhx <87083494+Noahhx@users.noreply.github.com>
2025-04-08 16:17:53 +0100

    Small Syntax Ambiguity Fix

--- Assets/OnHoverHelpFullGame.cs
+++ Assets/OnHoverHelpFullGame.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Diagnostics;
 using UnityEngine;
 
 public class OnHoverHelpFullGame : MonoBehaviour
@@ -8,12 +10,32 @@ public class OnHoverHelpFullGame : MonoBehaviour
     // Update is called once per frame
     void OnMouseOver()
     {
-        Debug.Log("Mouse is over the object");
+        UnityEngine.Debug.Log("Mouse is over the object");
         helpText.SetActive(true);
     }
     void OnMouseExit()
     {
-        Debug.Log("Mouse is no longer over the object");
+        UnityEngine.Debug.Log("Mouse is no longer over the object");
         helpText.SetActive(false);
     }
 }
+
+
+// property blueGroup[] = blueGroup.append(groupi[] = groupi[0] = property.NameProperty)
+
+
+// array = Array.Find(groups, g => g.StartsWith(property.Group));
+//array.append(property.NameProperty) (assuming passed by reference)
+
+
+// Switch(property.Group)
+// {
+//     case "blue":
+//        blueGroup.append(property.NameProperty);}
+
+//Switch(property.Group)
+//     case "red":
+//        redGroup.append(property.NameProperty);
+//     case "green":                
+//        greenGroup.append(property.NameProperty);
+//     case "yellow":
