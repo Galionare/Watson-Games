e412f87d71bc567f1a2fb538442a82a5947fa2ec
Galionare <galion1325@gmail.com>
2025-04-08 19:13:47 +0100

    .

--- Assets/SCRIPT 2/Player.cs
+++ Assets/SCRIPT 2/Player.cs
@@ -4,4 +4,5 @@ public class Player : MonoBehaviour
 {
     public int playerIndex; // Unique index for each player
     public string playerName; // Optional: to store player's name
+    public int money = 1500;
 }
