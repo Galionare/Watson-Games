08449da6c5fcc7cc4e81918b27294bac989e20d8
emma8753 <emma8753@outlook.com>
2025-04-11 12:19:35 +0100

    Updated propertyData dictionary

--- Assets/SCRIPT 2/PropertyData.cs
+++ Assets/SCRIPT 2/PropertyData.cs
@@ -22,7 +22,7 @@ public class PropertyData
     public string CostHouse;
     public string CostHotel;
 
-    public PropertyData(int position, string nameproperty, string group, string action, bool canBeBought, int cost, int mortgage, double returnMortgage, int rent, int fullRent,  int[] houses, string statRent1, string statRent2, string statRent3, string statRent4, string utilRent1, string utilRent2, string costHouse, string costHotel)
+    public PropertyData(int position, string nameproperty, string group, string action, bool canBeBought, int cost, int mortgage, double returnMortgage, int rent, int fullRent,  int[] houses, string statRent1, string statRent2, string statRent3, string statRent4, string utilRent1, string utilRent2, string costHouse, string costHotel, bool mortgaged, bool rentCollect, int ownerIndex, int numOfHouses)
     {
         Position = position;
         NameProperty = nameproperty;
@@ -43,5 +43,10 @@ public class PropertyData
         ReturnMotrtgage = returnMortgage;
         CostHouse = costHouse;
         CostHotel = costHotel;
+
+        Mortgaged = mortgaged;
+        RentCollect = rentCollect;
+        OwnerIndex = ownerIndex;
+        NumOfHouses = numOfHouses;
     }
 }
