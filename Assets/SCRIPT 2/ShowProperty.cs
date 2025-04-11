a3b51566c8ac457d6229266c7a8db0da2ff0a9ee
JakobHS <sharpejh00@gmail.com>
2025-04-11 14:38:45 +0200

    f

--- Assets/SCRIPT 2/ShowProperty.cs
+++ Assets/SCRIPT 2/ShowProperty.cs
@@ -1,48 +1,48 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class ShowProperty : MonoBehaviour
 {
-    public GameObject PropCard;
-    public GameObject StatCard;
-    public GameObject UtilCard;
-    public GameObject Canvas;
+     public GameObject PropCard;
+     public GameObject StatCard;
+     public GameObject UtilCard;
+     public GameObject Canvas;
 
-    private Dictionary<int, PropertyData> propertyData;
+     private Dictionary<int, PropertyData> propertyData;
 
-    public void ShowProp(int position)
-    {
-        int Position = position + 1;
-        Debug.Log(Position);
-        propertyData = CSVLoader.LoadPropertyData();
-        if (propertyData.TryGetValue(Position, out PropertyData data))
-        {
+     public void ShowProp(int position)
+     {
+         int Position = position + 1;
+         Debug.Log(Position);
+         propertyData = CSVLoader.LoadPropertyData();
+         if (propertyData.TryGetValue(Position, out PropertyData data))
+         {
 
-            if (data.CanBeBought && (data.Group.Contains("Brown") || data.Group.Contains("Blue") || data.Group.Contains("Purple") || data.Group.Contains("Orange") || data.Group.Contains("Red") || data.Group.Contains("Yellow") || data.Group.Contains("Green") || data.Group.Contains("Deep blue")))
-            {
-                GameObject Card1 = Instantiate(PropCard, Canvas.transform) as GameObject;
-                Card1.GetComponentInChildren<FullStreetCard>().CreateCard(Position);
-                FullStreetCard pos = Card1.GetComponentInChildren<FullStreetCard>();
-                pos.Position = Position;
-            }
-            if (data.CanBeBought && (data.Group.Contains("Station")))
-            {
-                GameObject Card2 = Instantiate(StatCard, Canvas.transform) as GameObject;
-                Card2.GetComponentInChildren<FullStationCard>().CreateCard(Position);
-                FullStationCard pos = Card2.GetComponentInChildren<FullStationCard>();
-                pos.Position = Position;
+             if (data.CanBeBought && (data.Group.Contains("Brown") || data.Group.Contains("Blue") || data.Group.Contains("Purple") || data.Group.Contains("Orange") || data.Group.Contains("Red") || data.Group.Contains("Yellow") || data.Group.Contains("Green") || data.Group.Contains("Deep blue")))
+             {
+                 GameObject Card1 = Instantiate(PropCard, Canvas.transform) as GameObject;
+                 Card1.GetComponentInChildren<FullStreetCard>().CreateCard(Position);
+                 FullStreetCard pos = Card1.GetComponentInChildren<FullStreetCard>();
+                 pos.Position = Position;
+             }
+             if (data.CanBeBought && (data.Group.Contains("Station")))
+             {
+                 GameObject Card2 = Instantiate(StatCard, Canvas.transform) as GameObject;
+                 Card2.GetComponentInChildren<FullStationCard>().CreateCard(Position);
+                 FullStationCard pos = Card2.GetComponentInChildren<FullStationCard>();
+                 pos.Position = Position;
 
-            }
+             }
 
-            if (data.CanBeBought && (data.Group.Contains("Utilities")))
-            {
-                GameObject Card3 = Instantiate(UtilCard, Canvas.transform) as GameObject;
-                Card3.GetComponentInChildren<FullUtilityCard>().CreateCard(Position);
-                FullUtilityCard pos = Card3.GetComponentInChildren<FullUtilityCard>();
-                pos.Position = Position;
+             if (data.CanBeBought && (data.Group.Contains("Utilities")))
+             {
+                 GameObject Card3 = Instantiate(UtilCard, Canvas.transform) as GameObject;
+                 Card3.GetComponentInChildren<FullUtilityCard>().CreateCard(Position);
+                 FullUtilityCard pos = Card3.GetComponentInChildren<FullUtilityCard>();
+                 pos.Position = Position;
 
-            }
-        }
-    }
+             }
+         }
+     }
+ 
 }
